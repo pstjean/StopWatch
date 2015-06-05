477b97aab150646285557fdf35a58ca5d626dd06
Peter St.Jean <pstjean@gmail.com>
2015-06-04 21:17:18 -0500

    Base our calculations off of system time instead of using our own internal counter

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,15 +1,25 @@
 var currentTime = 0;
 var tickyClock = false;
+var startDate = 0;
+var pauseDate = 0;
+var resumeDiff = 0;
 
 function updateWatch() {
+  //currentTime += 100;
+  currentTime = Date.now() - startDate - resumeDiff;
   $("#watchFace").html(
     getDisplayTime(currentTime)
   );
-  currentTime += 100;
 }
 
 function getDisplayTime(currentTime) {
-  currentTime = currentTime/1000;
+  // 66054
+  currentTime = currentTime/100;
+  // 660.54
+  currentTime = Math.round(currentTime);
+  // 661
+  currentTime = currentTime/10;
+  // 66.1
   currentTime = currentTime.toFixed(1);
   return currentTime;
 }
@@ -17,17 +27,32 @@ function getDisplayTime(currentTime) {
 $(document).ready(function() {
   $("#startWatch").click(function() {
     if(!tickyClock) {
+      if(!startDate) {
+        startDate = Date.now();
+      } else {
+        // Since we're keying off the current date to calculate current time
+        // we'll need to calculate the amount of time we pause the clock as well.
+        // We'll store the date when we pause and then figure out how long we paused
+        // when we resume by subtracting that date from the current one.
+        resumeDiff = Date.now() - pauseDate + resumeDiff;
+      }
+      // Run updateWatch every 100ms
       tickyClock = window.setInterval(updateWatch, 100);
     }
     $(this).prop("disabled", true);
   });
+  
   $("#stopWatch").click(function() {
     clearInterval(tickyClock);
+    pauseDate = Date.now();
     tickyClock = false;
     $("#startWatch").prop("disabled", false);
   });
+  
   $("#clearWatch").click(function() {
     currentTime = 0;
+    startDate = 0;
+    resumeDiff = 0;
     $("#watchFace").html(getDisplayTime(currentTime));
   });
 });

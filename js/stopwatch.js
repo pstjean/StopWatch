9beccbac1eed2daf4022e5c567731dd1878fbc4d
Peter St.Jean <pstjean@gmail.com>
2015-06-04 19:34:07 -0500

    Counting is working

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,5 +1,13 @@
+var currentTime = 0;
+
+function updateWatch() {
+  $("#watchFace").html(currentTime/1000);
+  currentTime += 100;
+}
+
 $(document).ready(function() {
     $("#startWatch").click(function() {
+      var tickyClock = window.setInterval(updateWatch, 100);
       alert('Started');
     });
     $("#stopWatch").click(function() {
